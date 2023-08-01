9d9eca9b249750f9e1224182f020483731eea18e
suresh33661 <sureshpragada3366@gmail.com>
2023-08-01 11:12:01 +0530

    .

---
+++ .idea/color.py
@@ -0,0 +1,6 @@
+import turtle
+
+form turtle
+a = turtle.Turtle()
+a.shape("elephant")
+a.color("orange")
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from turtle import Turtle
 a = Turtle()
 #a.shape("turtle")
 #a.color("red")
+a.color("red")
 for _ in range(4):
     a.forward(100)
     a.left(90)
+Turtle.done()
